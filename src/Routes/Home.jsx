cf40698a2396ae9f67d11af17c8cfd92ec6fe990
Kristian Kodehode <kristian007.kodehode@gmail.com>
2023-11-01 14:20:18 +0100

    adjusted beskrivelse class

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -8,10 +8,12 @@ const Home = () => {
           basically får øvd litt på å jobbe med å bruke github i en mer
           real-life-setting mtp å forke/pulle ut en branch, requests, merge inn
           igjen etc...gjerne også kjøre requeste "code-review" før merge osv.
+          <br /> <br />
           Det vi lager inni her er irrelevant tenker jeg, og trenger ikke å være
           noe spesifikt og heller ikke noe heavy shit siden formålet er mer å
-          bare bli vandt med å jobbe med egne branches osv på github :) <br />{" "}
-          <br /> <strong>Vite - React - Router - Sass (scss)</strong>.
+          bare bli vandt med å jobbe med egne branches og derettre merge det inn
+          osv på github :) <br /> <br />{" "}
+          <strong>Vite - React - Router - Sass (scss)</strong>.
         </p>
         <ol>
           <li>
