cfbcac96c86624534573de39947be8af5035aeaa
Kristian Kodehode <kristian007.kodehode@gmail.com>
2023-11-01 13:51:34 +0100

    removed imports not needed

--- src/Routes/Kristian/KristianRoute.jsx
+++ src/Routes/Kristian/KristianRoute.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import styles from "./styleKristian.module.scss";
 
 const KristianRoute = () => {
--- src/Routes/NavBar.jsx
+++ src/Routes/NavBar.jsx
@@ -1,6 +1,4 @@
-import React from "react";
 import { Link } from "react-router-dom";
-import KristianRoute from "./Kristian/KristianRoute";
 
 const NavBar = () => {
   return (
